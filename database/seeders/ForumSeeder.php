b2b5dedb3f9902ba3c890775dacf245144c01368
Ruben <tudorruben215@gmail.com>
2026-04-22 14:57:59 +0300

    simple category and forum seeder

--- database/seeders/ForumSeeder.php
+++ database/seeders/ForumSeeder.php
@@ -13,14 +13,38 @@ class ForumSeeder extends Seeder
      */
     public function run(): void
     {
-        $categories = ['Game Updates', 'Games', 'Community Originals', 'General'];
 
-        foreach($categories as $title) {
-            ForumCategory::create(['title' => $title]);
+        $data = [
+            'Game Updates' => [
+                'is_admin_only' => true,
+                'forums' => ['Announcements', 'Patch Notes']
+            ],
+            'Games' => [
+                'is_admin_only' => false,
+                'forums' => ['General Discussion', 'Guides & Tutorials', 'Feedback & Suggestions', 'Technical Support']
+            ],
+            'Community Originals' => [
+                'is_admin_only' => false,
+                'forums' => ['Fan Art', 'Lore & Fan Fiction', 'Modding & Assets', 'Media & Clips']
+            ],
+            'General' => [
+                'is_admin_only' => false,
+                'forums' => ['Casual Discussion', 'Off-Topic', 'Forum Games', 'Forum Feedback']
+            ],
+        ];
+
+        foreach($data as $title => $details) {
+            $category = ForumCategory::create([
+                'title' => $title,
+                'is_admin_only' => $details['is_admin_only'],
+            ]);
+
+            foreach($details['forums'] as $forumTitle) {
+                $category->forums()->create([
+                    'title' => $forumTitle,
+                ]);
+            }
         }
-
-
-
 
     }
 }
